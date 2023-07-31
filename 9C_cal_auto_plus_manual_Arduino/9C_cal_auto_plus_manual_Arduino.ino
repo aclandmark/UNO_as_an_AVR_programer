89bc049f334eb3c4d5ac87a0525e6d37a527a037
aclandmark <mark.osborne15@btinternet.com>
2023-07-31 17:38:02 +0100

    Update 9C_cal_auto_plus_manual_Arduino.ino

--- 9C_cal_auto_plus_manual_Arduino/9C_cal_auto_plus_manual_Arduino.ino
+++ 9C_cal_auto_plus_manual_Arduino/9C_cal_auto_plus_manual_Arduino.ino
@@ -1,6 +1,6 @@
 
 
-PRINT OUT PREVIOUS VALUS OF OSCALL
+//PRINT OUT PREVIOUS VALUS OF OSCALL
 
 
 /*
@@ -28,41 +28,45 @@ int main (void)
   int EEP_MAX = 0x2000;
   
   setup_HW;
-  OCR1A = 0xFFFF;
-  //ASSR = (1 << AS2);                                //initialise T2 for crystal
- 
+  //OCR1A = 0xFFFF;
+   if(device_calibrated)
+   User_cal_old = eeprom_read_byte((uint8_t*)(EE_size - 2)); 
+   else User_cal_old = 0;
+    
   sendString("\r\nTest string printed using default cal:  Can it be read?\r\n"); 
   
-  OSCCAL_DV = OSCCAL;                               //Save default value off OSCCAL
+  OSCCAL_DV = OSCCAL;                                           //Save default value off OSCCAL
 
-  Auto_cal (0);                                 //Search starting from 0xF0
-  OSCCAL += 1;                                  //Check performance for OSCCAL_WV +/- 1
-  //Get_ready_to_calibrate;
+  Auto_cal (0);                                                 //Search starting from 0xF0
+  OSCCAL += 1;                                                  //Check performance for OSCCAL_WV +/- 1
   sei();
-  error_up = compute_error_UNO(0,2,0);                        //Check that OSCCAL_WV is not at a
-  OSCCAL -=2; error_down = compute_error_UNO(0,2,0);                  //discontinuity on the
-  //close_calibration;                                //OSCCAL v error plot
+  error_up = compute_error_UNO(0,2,0);                          //Check that OSCCAL_WV is not at a
+  OSCCAL -=2; error_down = compute_error_UNO(0,2,0);            //discontinuity on the
   cli();
-  OSCCAL += 1;                                  //Restore OSCCAL to OSCCAL_WV
-  if ((error_up > 500) || (error_down > 500))             //1000
-  {sendString("\r\nPoor result: Searching for alternative value\r\n");
-  Auto_cal (1);}                                  //Repeat search starting from 0x10
+  OSCCAL += 1;                                                  //Restore OSCCAL to OSCCAL_WV
+  if ((error_up > 500) || (error_down > 500))
+  {sendString
+  ("\r\nPoor result: Searching for alternative value\r\n");
+  Auto_cal (1);}                                                //Repeat search starting from 0x10
   
   set_device_type_and_memory_size;
   sendString("\r\n\r\nCalibrating Atmega ");
   sendString (Device_type[device_ptr]);
   
-  save_cal_values(OSCCAL_WV);                           //Save result of the auto calibration
+  save_cal_values(OSCCAL_WV);                                   //Save result of the auto calibration
 
   sendString("\r\nNew OSCCAL value ");
   Num_to_PC(10,OSCCAL_WV);
 
-  sendString("\r\nPress 'x' to finish or AOK for manual cal\r\n");        //Omit for 4K device
-  if (waitforkeypress() == 'x')                         //Omit for 4K device
+if( User_cal_old){sendString("\r\nPrevious user cal ");
+Num_to_PC(10,User_cal_old);}
+
+  sendString
+  ("\r\nPress 'x' to finish or AOK for manual cal\r\n"); 
+  if (waitforkeypress() == 'x') 
     {printout_cal_values();}
   
-
-  else Manual_cal();                              //Omit for 4K device
+  else Manual_cal();
 
   sendString("\r\nAll done: AK to repeat.\r\n");
 
@@ -80,14 +84,10 @@ void Auto_cal (char direction){
   long  error_mag;
   int limit;
 
-  UCSR0B &= (~(1 << RXEN0));                        //avoid unwanted keypresses
-
-  //Timer_T1_sub(T1_delay_1sec);                      //Crystal warm up time
-
+  UCSR0B &= (~(1 << RXEN0));                                     //avoid unwanted keypresses
   sei();
   cal_mode = 2;
-  //Get_ready_to_calibrate;
-  
+ 
   if (!(direction))
     {counter_1 = 0xF1;
     while(1){sendChar('.');
@@ -97,7 +97,7 @@ void Auto_cal (char direction){
       if(error_mag < 1000)break;}
     OSCCAL_mem = OSCCAL;
     counter_2 = 0;
-    cal_mode = 2;   //5
+    cal_mode = 2; 
     limit = 1000;
     for(int m = 1; m <= 9; m++){sendChar('.');
       limit -= 100;
@@ -122,8 +122,6 @@ void Auto_cal (char direction){
   
   error_mag = compute_error_UNO(0,cal_mode,0);
   OSCCAL_WV = OSCCAL;
-  //close_calibration;
-//Num_to_PC(10, error_mag);sendChar('A');Num_to_PC(10, OSCCAL_mem);
   UCSR0B |= (1 << RXEN0);
 cli();}
 
@@ -137,27 +135,23 @@ void Manual_cal(void){
   unsigned char osccal_MIN;
   long percentage_error;
 
-  UCSR0B &= (~(1 << RXEN0));                          //avoid unwanted keypresses
-  sendString("\r\nManual cal: please wait 5 seconds\r\n");
-  Timer_T0_sub(T0_delay_5ms);                          //gives printer time to finish
+  UCSR0B &= (~(1 << RXEN0));                                    //avoid unwanted keypresses
+  sendString
+  ("\r\nManual cal: please wait 5 seconds\r\n");
+  Timer_T0_sub(T0_delay_5ms);                                   //gives printer time to finish
 
   sei();
-
   EA_buff_ptr = 0;
-  cal_mode = 2;//5;
-  ////Get_ready_to_calibrate;
+  cal_mode = 2;
   OSCCAL -=20;
-  osccal_MIN = OSCCAL;                            //Compute cal error for 41 values of OSCCAL
+  osccal_MIN = OSCCAL;                                          //Compute cal error for 41 values of OSCCAL
   for(int m = 0; m <= 40; m++)
   {cal_error = compute_error_UNO(1,cal_mode,1);OSCCAL++;}
-  OSCCAL = OSCCAL_WV;                             //Restore working value of OSCCAL
-  //close_calibration;
-
+  OSCCAL = OSCCAL_WV;                                           //Restore working value of OSCCAL
+  
   newline();
   sendString("OSCCAL\t Error\r\n");
-
-
-  for(int m = 0; m <= 40; m++)                        //Print table of values
+  for(int m = 0; m <= 40; m++)                                    //Print table of values
   {Num_to_PC(10,osccal_MIN); osccal_MIN++;
     sendChar('\t');Num_to_PC(10,buffer[m]);
     sendChar('\t');
@@ -165,80 +159,51 @@ void Manual_cal(void){
     Num_to_PC(10,percentage_error*100/32768);sendChar('%');
     newline();
     Timer_T0_sub(T0_delay_20ms);
-  Timer_T0_sub(T0_delay_20ms);}                       //stops PC being overwhelmed with data
+  Timer_T0_sub(T0_delay_20ms);}                                   //stops PC being overwhelmed with data
 
   sendString("\r\nEnter new user cal value or x to escape.");
 
-
-  UCSR0B |= (1 << RXEN0);                           //keypresses required again
+  UCSR0B |= (1 << RXEN0);                                         //keypresses required again
 
   if ((OSCCAL_UV = UC_from_KBD()) == 'x')
-    OSCCAL_UV = OSCCAL_WV;                          //stick with result of auto cal
+    OSCCAL_UV = OSCCAL_WV;                                        //stick with result of auto cal
 
   
   /*********************************************/
   else
-    {//Get_ready_to_calibrate;                        //Test value of OSCCAL entered by user
-    OSCCAL = OSCCAL_UV;                           //Test new OSCCAL value
+    {
+    OSCCAL = OSCCAL_UV;                                           //Test new OSCCAL value
     calibrate_without_sign_plus_warm_up_time;
-    //close_calibration;
-
-    if(cal_error > 1750)                          //Error resulting from User OSCCAL exceeds 1750
-      {OSCCAL = OSCCAL_WV;                        //Reinstate working value
+    
+    if(cal_error > 1750)                                          //Error resulting from User OSCCAL exceeds 1750
+      {OSCCAL = OSCCAL_WV;                                        //Reinstate working value
       OSCCAL_UV = OSCCAL_WV;
       sendString("\tChange rejected: error too great");}}
-
-
     cli();
     save_cal_values(OSCCAL_UV);
   printout_cal_values();}
 
 
 
-
-
-
-
-  /*************************************************************************************************************************************/
- /* ISR(TIMER2_OVF_vect) {                      //For use with watch Xtal
-    long TCNT1_BKP, overflow = 0, target_res;
-
-    target_res = 62500;
-    TCCR1B = 0;                                 //Halt T1
-    TCNT1_BKP = TCNT1;                              //Copy the value of TCNT1
-    TCNT1 = 0;                                  //Clear TCNT1
-    TCCR1B = 1;                                 //Get T1 running again ASAP (Note T2 has not stopped running)
-    
-    if(EA_counter < cal_mode)T1_OVF = 0;                     //Ignore first few results
-    else
-    {
-      switch(T1_OVF){
-        case 0: overflow = 0; break;
-        case 1: overflow = 65536; break;
-      case 2: overflow = 131072; break;}
-      error_SUM = error_SUM + (TCNT1_BKP - target_res + overflow);
-    T1_OVF = 0;}
-  EA_counter++;}
-
-*/
-
-
-ISR (PCINT0_vect){                       //UNO provides time standard   Pin change interrupt on SCK pin
+ /*************************************************************************************************************************************/
+ ISR (PCINT0_vect){                                               //UNO provides time standard   Pin change interrupt on SCK pin
                     
       if (!(TCCR1B)) {
       TCNT1_sum = 0;
       TCNT1 = 0;
-      TCCR1B = prescaller_setting;}             //1MHz clock
+      TCCR1B = prescaller_setting;}                               //1MHz clock
   
     else {TCCR1B = 0; int_counter += 1;
-      error_sum = error_sum + TCNT1_sum - 32768 + TCNT1;
-    }}
+      error_sum = error_sum + TCNT1_sum - 32768 + TCNT1;}}
  
 
 
 
   /************************************************************************************************************************************/
-//ISR(TIMER1_OVF_vect) {T1_OVF += 1;}       //For use with watch Xtal
-ISR(TIMER1_COMPA_vect){TCNT1_sum += 0x100; }    //UNO provides time standard
+
+
+
+
+
 
   /************************************************************************************************************************************/
