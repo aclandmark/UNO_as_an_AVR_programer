d2f1b207416c0c1042a48e4218aee128bcf305d8
aclandmark <mark.osborne15@btinternet.com>
2023-08-04 20:06:04 +0100

    Update 9C_cal_auto_plus_manual_Arduino.ino

--- 9C_cal_auto_plus_manual_Arduino/9C_cal_auto_plus_manual_Arduino.ino
+++ 9C_cal_auto_plus_manual_Arduino/9C_cal_auto_plus_manual_Arduino.ino
@@ -56,23 +56,13 @@ int main (void)
   save_cal_values(OSCCAL_WV);                                   //Save result of the auto calibration
 
   sendString("\r\nNew OSCCAL value ");
-  Num_to_PC(10,OSCCAL_WV);
+  //Num_to_PC(10,OSCCAL_WV);//
 
 if( User_cal_old){sendString("\r\nPrevious user cal ");
-Num_to_PC(10,User_cal_old);}
-
-  sendString
-  ("\r\nPress 'x' to finish or AOK for manual cal\r\n"); 
-  if (waitforkeypress() == 'x') 
-    {printout_cal_values();}
-  
-  else Manual_cal();
-
-  sendString("\r\nAll done: AK to repeat.\r\n");
-
-  waitforkeypress();
-
-SW_reset;}
+//Num_to_PC(10,User_cal_old);
+}
+while(1);
+}
 
 
 
@@ -130,58 +120,7 @@ cli();}
 
 
 /**********************************************************************************************************************/
-void Manual_cal(void){
-  long cal_error;
-  unsigned char osccal_MIN;
-  long percentage_error;
 
-  UCSR0B &= (~(1 << RXEN0));                                    //avoid unwanted keypresses
-  sendString
-  ("\r\nManual cal: please wait 5 seconds\r\n");
-  Timer_T0_sub(T0_delay_5ms);                                   //gives printer time to finish
-
-  sei();
-  EA_buff_ptr = 0;
-  cal_mode = 2;
-  OSCCAL -=20;
-  osccal_MIN = OSCCAL;                                          //Compute cal error for 41 values of OSCCAL
-  for(int m = 0; m <= 40; m++)
-  {cal_error = compute_error_UNO(1,cal_mode,1);OSCCAL++;}
-  OSCCAL = OSCCAL_WV;                                           //Restore working value of OSCCAL
-  
-  newline();
-  sendString("OSCCAL\t Error\r\n");
-  for(int m = 0; m <= 40; m++)                                    //Print table of values
-  {Num_to_PC(10,osccal_MIN); osccal_MIN++;
-    sendChar('\t');Num_to_PC(10,buffer[m]);
-    sendChar('\t');
-    percentage_error = buffer[m];
-    Num_to_PC(10,percentage_error*100/32768);sendChar('%');
-    newline();
-    Timer_T0_sub(T0_delay_20ms);
-  Timer_T0_sub(T0_delay_20ms);}                                   //stops PC being overwhelmed with data
-
-  sendString("\r\nEnter new user cal value or x to escape.");
-
-  UCSR0B |= (1 << RXEN0);                                         //keypresses required again
-
-  if ((OSCCAL_UV = UC_from_KBD()) == 'x')
-    OSCCAL_UV = OSCCAL_WV;                                        //stick with result of auto cal
-
-  
-  /*********************************************/
-  else
-    {
-    OSCCAL = OSCCAL_UV;                                           //Test new OSCCAL value
-    calibrate_without_sign_plus_warm_up_time;
-    
-    if(cal_error > 1750)                                          //Error resulting from User OSCCAL exceeds 1750
-      {OSCCAL = OSCCAL_WV;                                        //Reinstate working value
-      OSCCAL_UV = OSCCAL_WV;
-      sendString("\tChange rejected: error too great");}}
-    cli();
-    save_cal_values(OSCCAL_UV);
-  printout_cal_values();}
 
 
 
